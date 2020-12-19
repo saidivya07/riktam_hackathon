f12a744a984a0e8117488de08c60397049833b8c
saidivya07 <54140272+saidivya07@users.noreply.github.com>
2020-12-19 15:13:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,14 +12,88 @@
 		<!-- Header starts here -->
 		<div id="header">
 			<img src="images/img2.jpg" style="float:left;"/>
+			<form method="post" action="" id="form1">
+				<strong>Email:</strong>
+				<input type="email" name="email" placeholder="Email" required="required"/>
+				<strong> Password:</strong>
+				<input type="password" name="pass" placeholder="********" required="required"/>
+				<button name="login">Login</button>
+
 
 		</div>
 		<!-- Header ends here -->
 	</div>
 	<!-- Header wrapper ends here -->
 
+	<div id="content"><!-- Content Area Starts here -->
+		<div>
+			<h2><i> Vow for a Change within the Community!</i></h2>
+			<img src="images/img1.png" styles="float:left; margin-left:-40px"/>
+		</div>
+
+		<div id="form2" styles="float:left; margin-left: 400px">
+		
+			<form action="" method="post">
+				<h2>Register Today...</h2>
+				<sub>Contribute towards the betterment of the community!</sub>
+				<table>
+					<tr>
+					<td align="right"><strong>Name</strong></td>
+					<td><input type="text" name="u_name" required="required" placeholder="Enter your name"/></td>
+					</tr>
+					
+					<tr>
+					<td><strong>Email Id</strong></td>
+					<td><input type="email" name="u_email" required="required" placeholder="Enter your Email"/></td>
+					</tr>
+
+					<tr>
+					<td><strong>Password</strong></td>
+					<td><input type="password" name="u_pass" required="required" placeholder="Create password"/></td>
+					</tr>
+
+					<tr>
+					<td><strong>Country</strong></td>
+					<td><input type="text" name="u_country" required="required" placeholder="Your Country Name"/></td>
+					</tr>
+
+					<tr>
+					<td><strong>Gender</strong></td>
+					<td><select name="u_gender">
+						<option>Male</option>
+						<option>Female</option>
+						<option>Dont want to say</option>
+					</select>
+					</td>
+					</tr>
+
+					<tr>
+					<td><strong>Birthday</strong></td>
+					<td><input type="date" name="u_birthday" required="required"/></td>
+					</tr>
+
+					<tr>
+						<td>
+							<button name="sign_up"> Sign Up</button>
+						</td>
+					</tr>
+				</table>
+
+			</form>
+			
+		</div>
+		<!-- Content Area Starts here -->
+
 
 </div>
+
 <!--Container ends from here -->
+
+<!--footer starts-->
+
+<div id="footer">
+	<h2>&copy; SAI DIVYA NALLAPANENI</h2>
+</div>
+<!-- Footer Ends-->
 </body>
 </html>
