94a9ba6dfb44861ef35c6e42d064b37652f3fc2d
saidivya07 <54140272+saidivya07@users.noreply.github.com>
2020-12-19 13:28:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Civic Problems Finder!</title>
+<link rel="stylesheet" href="styles/style.css" media="all"/>
+</head>
+<body>
+<!--Container starts from here -->
+<div class="container">
+	<!-- Header wrapper starts here -->
+	<div id="header_wrapper">
+		<!-- Header starts here -->
+		<div id="header">
+			<img src="images/img2.jpg" style="float:left;"/>
+
+		</div>
+		<!-- Header ends here -->
+	</div>
+	<!-- Header wrapper ends here -->
+
+
+</div>
+<!--Container ends from here -->
+</body>
+</html>
