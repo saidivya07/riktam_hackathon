0377c811b0fb8d91c592bce68511d38637cf19f6
saidivya07 <54140272+saidivya07@users.noreply.github.com>
2020-12-20 11:30:31 +0530

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -93,6 +93,91 @@ function get_posts()
 	include("pagination.php");
 }
 
+function single_post(){
+	if(isset($_GET['post_id'])){
+		global $con;
+		$get_id=$_GET['post_id'];
+		$get_posts="select * from posts where post_id='$get_id'";
+		$run_posts =mysqli_query($con,$get_posts);
+		$row_posts = mysqli_fetch_array($run_posts);
+		$post_id=$row_posts['post_id'];
+		$user_id=$row_posts['user_id'];
+		$post_title = $row_posts['post_title'];
+		$post_content=$row_posts['post_content'];
+		$post_date=$row_posts['post_date'];
+		//getting the user who has posted the thread
+		$user = "select * from users where user_id='$user_id'";
+		$run_user = mysqli_query($con,$user);
+		$row_user = mysqli_fetch_array($run_user);
+		$user_name = $row_user['user_name'];
+		$user_image = $row_user['user_image'];
+		//getting the user session
+		$user_com=$_SESSION['user_email'];
+		$get_com="select * from users where user_email='$user_com'";
+		$run_com=mysqli_query($con,$get_com);
+		$row_com=mysqli_fetch_array($run_com);
+		$user_com_id =$row_com['user_id'];
+		$user_com_name= $row_com['user_name'];
+		//now displaying all at once
+		echo "<div id='posts'>
+		<p><img src='users/$user_image' width='50' height='50'/></p>
+		<h3><a href='user_profile.php?u_id=$user_id'>$user_name</a></h3>
+		<h3>$post_title</h3>
+		<h4>$post_date</h4>
+		<h5>$post_content</h5>
+		
+		</div>";
+		include("comments.php");
+		echo "<form action='' method='post' id='reply'>
+		<textarea cols='50' rows='5' name='comment' placeholder='Write your opinions'></textarea><br/>
+		<input type='submit' name='reply' value='Reply to This'/>
+		</form>
+		";
+		if(isset($_POST['reply'])){
+			$comment = $_POST['comment'];
+			$insert = "insert into comments 
+			(post_id,user_id,comment,comment_author,date) values ('$post_id','$user_id','$comment','$user_com_name',NOW())";
+			$run =mysqli_query($con,$insert);
+			echo "<h2>Your Reply was added</h2>";
+		}
 
+
+	}
+
+
+	}
+
+	function members(){
+		global $con;
+		//select new members
+		$user="select * from users";
+		$run_user=mysqli_query($con,$user);
+		echo "<br/><h2>New Members found</h2><br>";
+		while ($row_user=mysqli_fetch_array($run_user)){
+		 $user_id=$row_user['user_id'];
+		 $user_name=$row_user['user_name'];
+		 $user_image = $row_user['user_image'];
+
+		 echo "
+		 <span>
+		 <a href='user_profile.php?u_id=$user_id'>
+
+		<img src= 'users/$user_image' width='50' height='50' title='$user_name' style='float:left;margin:10px;'/>
+		 </a>
+		 </span>";
+		}
+	}
+
+	//function for displaying user posts
+	function my_posts()
+	{
+		global $con;
+		if(isset($_GET['u_id'])){
+			$u_id=$_GET['u_id'];
+		}
+		$get_posts = "select * from posts where user_id='$u_id' ORDER by DESC LIMIT 5";
+		$run_posts = mysqli_query($con,$get_posts);
+		}
+	}
 ?>
 
