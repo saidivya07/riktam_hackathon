9c7166573496e7b4d8ad0867b7b9e9092c545b90
saidivya07 <54140272+saidivya07@users.noreply.github.com>
2020-12-20 08:10:09 +0530

    Update functions.php

--- functions/functions.php
+++ functions/functions.php
@@ -49,4 +49,50 @@ function insertPost()
 	    }
     }
 }
+
+
+//function for displaying posts
+function get_posts()
+{
+	global $con;
+	$per_page=5;
+	if(isset($GET['page']))
+	{
+		$page=$_GET['page'];
+	}
+	else
+	{
+		$page=1;
+	}
+	$start_from = ($page-1) * $per_page;
+	$get_posts = "select * from posts ORDER by 1 DESC LIMIT $start_from, $per_page";
+	$run_posts=mysqli_query($con,$get_posts);
+	while($row_posts=mysqli_fetch_array($run_posts)){
+		$post_id = $row_posts['post_id'];
+		$user_id = $row_posts['user_id'];
+		$post_title = $row_posts['post_title'];
+		$post_content = substr($row_posts['post_content'],0,150);
+		$post_date = $row_posts['post_date'];
+
+		//getting the user who has posted the thread
+		$user = "select * from users where user_id='$user_id'";
+		$run_user = mysqli_query($con,$user);
+		$row_user = mysqli_fetch_array($run_user);
+		$user_name = $row_user['user_name'];
+		$user_image = $row_user['user_image'];
+		//now displaying all at once
+		echo "<div id='posts'>
+		<p><img src='users/$user_image' width='50' height='50'/></p>
+		<h3><a href='user_profile.php?u_id=$user_id'>$user_name</a></h3>
+		<h3>$post_title</h3>
+		<h4>$post_date</h4>
+		<h5>$post_content</h5>
+		<a href='single.php?post_id=$post_id' style='float:right;'><button>See Replies or Reply to This</button></a>
+		</div><br/>";
+	}
+	include("pagination.php");
+}
+
+
 ?>
+
